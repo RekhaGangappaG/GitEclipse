f7ea4952f056e1cf30aa35993868a0caaa872856
RekhaGangappaG <grekha1997@gmail.com>
2019-05-30 16:47:46 -0700

    Employee model

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,27 @@
+
+public class Employee {
+
+	private int empid;
+	private String empno;
+	double sal;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpno() {
+		return empno;
+	}
+	public void setEmpno(String empno) {
+		this.empno = empno;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	
+	
+}
